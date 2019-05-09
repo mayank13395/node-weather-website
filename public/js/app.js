096a6a354ed97a076b749d2146416c4a59079e0e
unknown <kumar.mayank13395@gmail.com>
2019-05-09 14:22:36 +0530

    new heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', (e)=>{
     
     
      
-    fetch('http://localhost:3000/weather?address='+address).then( (response)=>{
+    fetch('/weather?address='+address).then( (response)=>{
     response.json().then((data)=>{
         console.log(data);
 
